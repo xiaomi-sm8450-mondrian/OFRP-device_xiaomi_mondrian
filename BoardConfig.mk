0cf3044d2e0e03fc43092ace2dbd7c013f628548
Keosh <106485828+keosh1@users.noreply.github.com>
2025-06-25 09:57:50 +0300

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -87,7 +87,7 @@ $(foreach p, $(BOARD_PARTITION_LIST), $(eval BOARD_$(p)IMAGE_FILE_SYSTEM_TYPE :=
 $(foreach p, $(BOARD_PARTITION_LIST), $(eval TARGET_COPY_OUT_$(p) := $(call to-lower, $(p))))
 
 # System as root
-BOARD_BUILD_SYSTEM_ROOT_IMAGE := false
+# BOARD_BUILD_SYSTEM_ROOT_IMAGE := false
 BOARD_ROOT_EXTRA_FOLDERS := bluetooth dsp firmware persist metadata
 BOARD_SUPPRESS_SECURE_ERASE := true
 
